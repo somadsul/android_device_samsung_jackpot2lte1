0cede098549aaab67cb11ebc202bc23ef19a5ab0
somadsul <somnathadsul@gmail.com>
2020-10-15 16:57:32 +0000

    update

--- lineage_jackpot2lte.mk
+++ lineage_jackpot2lte.mk
@@ -1,22 +1,9 @@
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/jackpot2lte/device.mk)
+$(call inherit-product, device/samsung/jackpot2lte/full_jackpot2lte.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := jackpot2lte
 PRODUCT_NAME := lineage_jackpot2lte
-PRODUCT_MODEL := SM-A730F
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
